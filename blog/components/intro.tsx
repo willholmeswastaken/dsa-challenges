ca6da09b18a0354638d1114d5f0286bcfc98e874
Will Holmes <willholmes97@icloud.com>
2023-10-08 20:20:37 +0100

    Added blog with initial blog post

---
+++ blog/components/intro.tsx
@@ -0,0 +1,14 @@
+const Intro = () => {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
+        Wills DSA Blog.
+      </h1>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+        A place to document my learnings with data structures and algorithms.
+      </h4>
+    </section>
+  )
+}
+
+export default Intro
